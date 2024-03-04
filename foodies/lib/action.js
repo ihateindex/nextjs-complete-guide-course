95ea970df9131677fcf0e5332ab61b950c5f59bb
ihateindex <47144148+ihateindex@users.noreply.github.com>
2024-03-04 14:58:51 +0900

    feat: 서버사이드에서 input 유효성 확인 기능 추가
    1. 비정상적인 input value들을 체크하고 문제 발견시 error페이지로 랜딩

--- foodies/lib/action.js
+++ foodies/lib/action.js
@@ -3,6 +3,11 @@ import { redirect } from 'next/navigation';
 import { saveMeal } from './meals';
 
 export async function shareMeal(formData) {
+    function inInvalidText(text) {
+        if (!text || text.trim() === '') {
+        }
+    }
+
     const meal = {
         title: formData.get('title'),
         summary: formData.get('summary'),
@@ -12,6 +17,19 @@ export async function shareMeal(formData) {
         creator_email: formData.get('email'),
     };
 
+    if (
+        inInvalidText(meal.title) ||
+        inInvalidText(meal.summary) ||
+        inInvalidText(meal.instructions) ||
+        inInvalidText(meal.image) ||
+        inInvalidText(meal.creator) ||
+        inInvalidText(meal.creator_email) ||
+        !meal.creator_email.includes('@') ||
+        !meal.image ||
+        meal.image.size === 0
+    ) {
+        throw new Error('Invalid input');
+    }
     await saveMeal(meal);
     redirect('/meals');
 }
