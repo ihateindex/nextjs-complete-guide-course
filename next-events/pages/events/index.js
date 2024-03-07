f9ce3407217178db6b528818d6110c48a3e7734b
ihateindex <47144148+ihateindex@users.noreply.github.com>
2024-03-07 14:57:34 +0900

    feat: 더미 데이터 및 정적 이미지 파일 추가
    1. dummy-data.js 추가
    2. public 폴더에 image들 추가

--- next-events/pages/events/index.js
+++ next-events/pages/events/index.js
@@ -1,7 +1,9 @@
-function EventsPage() {
-    return <div>
-        <h1>Events Page</h1>
-    </div>
+function AllEventsPage() {
+    return (
+        <div>
+            <h1>All Events Page</h1>
+        </div>
+    );
 }
 
-export default EventsPage
+export default AllEventsPage;
