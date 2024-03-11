8a60e072fb5d3f68675c151c3ad4c3e860094bfa
ihateindex <47144148+ihateindex@users.noreply.github.com>
2024-03-11 19:06:03 +0900

    feat: getStaticProps를 이용한 정적페이지 데이터 불러오기
    1. getStaticProps를 사용하여 build시에 페이지에 적용될 작업을 할 수 있다.
    2. getStaticProps는 return값에 props 객체를 무조건 리턴해야하며 이 객체는 컴포넌트의 props로 전달된다.
    3. getStaticProps에서 url등의 쿼리스트링 정보등은 context 파라미터를 사용하여 가져온다.
    4. return 객체에 revalidate라는 시간설정을 담당하는 프로퍼티를 넣어주면 해당 시간마다 정적페이지가 재갱신된다.

--- pre-rendering_data-fetching/pages/index.js
+++ pre-rendering_data-fetching/pages/index.js
@@ -1,11 +1,35 @@
+import path from 'path';
+import fs from 'fs/promises';
+
+import Link from 'next/link';
+
 function HomePage(props) {
-  return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
-  );
+    console.log(props);
+    const { products } = props;
+
+    return (
+        <ul>
+            {products.map((product) => {
+                return (
+                    <li key={product.id}>
+                        <Link href={`/${product.id}`}>{product.title}</Link>
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+
+export async function getStaticProps() {
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    return {
+        props: {
+            products: data.products,
+        },
+    };
 }
 
 export default HomePage;
